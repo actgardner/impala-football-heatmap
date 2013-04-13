75ac746799e78915b044327ab936d9c40007814b
A Gardner <3100188+actgardner@users.noreply.github.com>
2013-04-13 00:58:05 +0000

    Major overhaul to add multiple players and playback mode

--- public/impala_viz.js
+++ public/impala_viz.js
@@ -1,63 +1,130 @@
 var heatmap;
-var loading = new Spinner;
 var inprogress = false;
 var uptodate = true;
-var minTime=0, maxTime=80;
+var minScale=0, maxScale=80; // This is the scale itslef
+var minTime=0, maxTime=40; // default period
 var players;
+var playerList=[];
+var playerCount=0;
 
-window.onload = function(){
-    var config = {
-        element: document.getElementById("heatmap-canvas"),
-        radius: 40,
-        opacity: 80,
-        legend: {
-            position: 'br',
-            title: 'Football Position Heatmap'
-        }
+// The following represent the 0.5 and 1.0 weights for the heatmap, 
+//    per player
+var colourPool = [ ['rgb(255,127,127)','rgb(255,0,0)'], 
+                   ['rgb(127,255,127)','rgb(0,255,0)'], 
+                   ['rgb(127,127,255)','rgb(0,0,255)'],
+                   ['rgb(127,255,255)','rgb(0,255,255)'],
+                   ['rgb(255,127,255)','rgb(255,0,255)'],
+                   ['rgb(255,255,127)','rgb(255,255,0)']];
+
+var config = {
+        radius: 34,
+        opacity: 70,
     };
 
+var playbackTimer=false;
+
+var spin_opts = {
+  lines: 9, // The number of lines to draw
+  length: 5, // The length of each line
+  width: 3, // The line thickness
+  radius: 3, // The radius of the inner circle
+  corners: 0.7, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#000', // #rgb or #rrggbb
+  speed: 1, // Rounds per second
+  trail: 51, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: 'auto', // Top position relative to parent in px
+  left: 'auto' // Left position relative to parent in px
+}
+
+window.onload = function(){
     $('#time-range').slider({ 'range': true,
-                              'min': minTime,
-                              'max': maxTime,
+                              'min': minScale,
+                              'max': maxScale,
                               'values': [minTime, maxTime],
                               'slide' :function(event, ui){
                                    minTime = ui.values[0];
                                    maxTime = ui.values[1];
-                                   reqNewData(); 
+                                   updateAllPlayers(); 
                               }});
-    
-    heatmap = h337.create(config);
-    $('#player-select').change(function(e){
-                                   reqNewData();
-                               });
-    $('#sensor-select').change(function(e){
-                                   reqNewData();
-                               });
-    addRegularTicks($('#range-holder'), 0, 90, 10);
+    $('#add-player').click(addPlayerViz);
+    $('#play').click(startPlayback);
+    addRegularTicks($('#range-holder'), minScale, maxScale, 9);
     requestPlayers();
 };
 
+function removePlayer(player){
+    console.log(player.index);
+    player.wrapper.remove();
+    player.map.remove();
+    playerList.splice(player.index, 1);
+    colourPool.splice(0,0,player['colours']);
+    playerCount -= 1;
+}
+
+function addPlayerViz(){
+    var newPlayer = {};
+    newPlayer['loading'] = new Spinner(spin_opts);
+    newPlayer['colours'] = colourPool[0];
+    colourPool.splice(0,1);
+    newPlayer['select'] = $(document.createElement('select')).attr('disabled', 'true');
+    newPlayer['spin'] = $(document.createElement('div')).attr('class', 'colour-block');
+    newPlayer['colour'] = $(document.createElement('div')).attr('class', 'colour-block').css('background-color', newPlayer['colours'][1]);
+    newPlayer['wrapper'] = $(document.createElement('div'));
+    newPlayer['map'] = $(document.createElement('div')).attr('class', 'map-canvas');
+    newPlayer['index'] = playerCount;
+    newPlayer['delete'] = $(document.createElement('input')).attr('type', 'button').attr('value', 'Remove').click(function(){removePlayer(newPlayer);});
+    newPlayer['wrapper'].append(newPlayer['colour']);
+    newPlayer['wrapper'].append(newPlayer['select']);
+    newPlayer['wrapper'].append(newPlayer['delete']);
+    newPlayer['wrapper'].append(newPlayer['spin']);
+    var playerConf = config;
+    playerConf.gradient = {0.00:"white", 0.50:newPlayer['colours'][0], 1.00:newPlayer['colours'][1]};
+    playerConf.element = newPlayer['map'][0];
+    $('#player-list').append(newPlayer['wrapper']);
+    $('#heatmap-canvas').append(newPlayer['map']);
+    playerList[playerCount] = newPlayer;
+    playerCount += 1;
+    newPlayer['select'].change(function(){
+        populateHeatmap(newPlayer);    
+    });
+    newPlayer['h337'] = h337.create(playerConf);
+    populatePlayerList(newPlayer['select']);
+    populateHeatmap(newPlayer); 
+}
+
+function populatePlayerList(list){
+   var lastTeam = false;
+   list.attr('disabled', false);
+   optGroup = $(document.createElement('optgroup')).attr('label', "Other");
+   list.append(optGroup);
+   for ( var i in players ){
+        if ( players[i]['team'] !== lastTeam){
+            if ( players[i]['team'] ){
+               lastTeam = players[i]['team'];
+               optGroup = $(document.createElement('optgroup')).attr('label', "Team "+lastTeam);
+               list.append(optGroup);
+            }
+        }
+        optGroup.append(new Option(players[i]['player_name'], players[i]['sid']));
+    }
+}
+
 function requestPlayers(){
    $('#player-select').attr('disabled', true);
-   $('#sensor-select').attr('disabled', true);
    $.get('/players', populatePlayers); 
 }
 
 function populatePlayers(data){
-    players=data.players;
-    $('#player-select').attr('disabled', false);
-    $('#player-select').empty();
-    for ( var i in data['players'] ){
-        if ( data['players'][i]['sensor_position'] === "LL" ){
-            $('#player-select').append(new Option(data['players'][i]['player_name'], data['players'][i]['player_name']));        
-        }
-    } 
-    $('#sensor-select').attr('disabled', false);
-    $('#sensor-select').empty();
-    for ( var i in data['sensors'] ){
-        $('#sensor-select').append(new Option(data['sensors'][i], data['sensors'][i]));
-    }
-    reqNewData();
+   players=data;
+   for (var i in playerList){
+       populatePlayerList(playerList[i]['select']);
+   } 
 }
 
 function addRegularTicks(slider, min, max, ticks){
@@ -71,48 +138,58 @@ function addRegularTicks(slider, min, max, ticks){
     }
 }
 
-function reqNewData(){
-   if (inprogress){
-       uptodate = false;
-       return;
-   }
-   inprogress=true;
-   var e = loading.spin().el;
-   $(e).css('left', '50%');
-   $(e).css('top', '50%');
-   $('#heatmap-canvas').append(e);
-   $.get('/data', 
-         {'min': (minTime *60000000000000)  + 10629342490369879, // picoseconds to seconds, plus the offset 
-          'max': (maxTime *60000000000000)  + 10629342490369879, 
-          'sid': getSid($('#sensor-select').val(), $('#player-select').val())}, 
-          displayNewData ); 
+function updateAllPlayers(){
+    for (var p in playerList){
+        populateHeatmap(playerList[p]);
+    } 
 }
 
-function getSid(sensor_type, player_name){
-    console.log(sensor_type+" "+player_name);
-    for (var i in players){
-        if ( players[i].player_name === player_name 
-             && players[i].sensor_position === sensor_type){
-            return players[i].sid;
+function populateHeatmap(playerList){
+    if ( playerList['inprogress'] ){
+        playerList['buffer'] = true;
+        return;
+    }
+    playerList['inprogress'] = true;
+    var e = playerList['loading'].spin().el;
+    $(e).css('left', '50%');
+    $(e).css('top', '50%');
+    playerList['spin'].append(e);
+    $.get('/data',
+         {'min': (minTime *60000000000000)  + 10753295594424116, // picoseconds to seconds, plus the offset
+          'max': (maxTime *60000000000000)  + 10753295594424116,
+          'sid': playerList['select'].val()},
+          function(data) { displayNewData(playerList, data); } ); 
+}
+
+function displayNewData(playerList, data){
+    playerList['loading'].stop();
+    playerList['inprogress'] = false;
+    data = setCount(data);
+    var m = findMax(data);
+    playerList['h337'].store.setDataSet({max: m, data: data});
+    if ( playerList['buffer'] ) {
+        playerList['buffer'] = false;
+        populateHeatmap(playerList);
+    }
+    playerList['buffer'] = false;
+}
+
+function setCount(data){
+    for ( var i in data ){
+        // Kludge to support reporting the ball position
+        if ( $('#player-select').val() == 4 ){
+            data[i].count = data[i].val/10;
+        } else {
+            data[i].count = data[i].val;
         }
     }
-}
-
-function displayNewData(data){
-    loading.stop();
-    inprogress = false;
-    heatmap.store.setDataSet({max: findMax(data), data: data});
-    if ( ! uptodate ) {
-        uptodate = true;
-        reqNewData();
-    }
-    uptodate = true;
+    return data;
 }
 
 function findMax(data){
     var max=[0,0,0];
     for ( var i in data ){
-       max = addToList(data[i].val, max, 20); 
+       max = addToList(data[i].count, max, 20); 
     }
     console.log(max);
     return max[2];
@@ -129,4 +206,35 @@ function addToList(val, list, size){
       } 
    }
    return list;
+}
+
+var tickSpan = 0.5;
+
+function startPlayback(){
+    $('#time-range').slider().off('slide');
+    maxTime = minTime + tickSpan;
+    $('#time-range').slider('values', 1 , parseInt($('#time-range').slider('values', 0)) + tickSpan);
+    $('#play').off('click');
+    $('#play').click(stopPlayback); 
+    $('#play').val('Pause Playback');
+    playbackTimer = setInterval(playbackTick, 1000);   
+}
+
+function playbackTick(){
+    maxTime += tickSpan;
+    minTime += tickSpan;
+    $('#time-range').slider('values', 1, parseInt($('#time-range').slider('values', 1)) + tickSpan);
+    $('#time-range').slider('values', 0, parseInt($('#time-range').slider('values', 0)) + tickSpan);
+    if (maxTime > maxScale) {
+        stopPlayback();
+    } else {
+        updateAllPlayers();    
+    }
+}
+
+function stopPlayback(){
+    clearInterval(playbackTimer);
+    $('#play').val('Playback Game');
+    $('#play').off('click');
+    $('#play').click(startPlayback);
 }
--- web.rb
+++ web.rb
@@ -2,25 +2,30 @@ require 'sinatra'
 require 'json'
 require 'impala'
 
+set :bind, '0.0.0.0'
+set :port, 80
+
 IMPALA_SERVER=''
+
+get '/' do
+  redirect '/index.html'
+end
 
 get '/data' do
   content_type 'application/json'
   data=[]
   Impala.connect(IMPALA_SERVER, 21000) do |conn|
-    data = conn.query("select (round(x/2650)*17)+440 as x, (round(-1*y/3400)*24)+280 as y, count(*)/200 val from soccer_part WHERE sid =#{params[:sid]} AND ts < #{params[:max]} AND ts > #{params[:min]} group by x,y")
+    data = conn.query("select (round(-1*x/2624)*25)+563 as y, (round(-1*y/3400)*39)+472 as x, count(*)/200 as val from soccer_part WHERE sid =#{params[:sid]} AND ts <= #{params[:max]} AND ts >= #{params[:min]} AND x > 0 group by x,y")
   end
   return JSON.dump data
 end
 
 get '/players' do
   content_type 'application/json'
-  data = {:sensors=>['LL', 'RL']}
-  players = []
+  data=[]
   Impala.connect(IMPALA_SERVER, 21000) do |conn|
-    data = conn.query("select sid, sensor_position, player_name FROM sensors WHERE player_type='P'")
+    data = conn.query("SELECT sid, player_name, team AS team FROM sensors WHERE sensor_position in ('LL','B') ORDER BY team,player_type,player_name LIMIT 100")
   end
-  data = {:sensors=>['LL', 'RL'], :players=>data}
   return JSON.dump data
 end
 
